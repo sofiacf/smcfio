079141d9cd82a513aadff30a3f7a5b3bbf2b4f2a
sofia <sofia@smcf.io>
2019-04-16 17:03:00 -0400

    Add portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import '../styles/Resume.css';
+import '../styles/Portfolio.css';
 
 class Project extends Component {
   render() {
     return (
       <div>
-      <h3>{this.props.name}</h3>
-      <span>Technologies: {this.props.technologies} </span>
+      <h3><a href={this.props.url}>{this.props.name}</a></h3>
+      <span>{this.props.technologies}</span>
       <p>{this.props.description}</p>
       </div>
     );
@@ -18,7 +18,8 @@ class Portfolio extends Component {
     return (
       <div>
       <h1>Portfolio</h1>
-      <Project name="Courier Map" description="Built quote generation and subcontractor assignment dispatching tool for USA Couriers' nationwide courier network."/>
+      <Project name="Courier Map" url="https://smcf.nfshost.com/map/" technologies="JavaScript, Google Maps JavaScript API, MariaDB, PHP, HTML/CSS" description="Developed quote generator and subcontractor dispatch tool for national courier network. Harnessed data from decades of company’s experience and relationships, creating an interactive GUI and updatable database, enabling any user to optimize dispatch decisions." />
+      <Project name="Accounting Add-on" url="https://github.com/sofiacf/osc-billing" technologies="Google Apps Script" description="Developed quote generator and subcontractor dispatch tool for national courier network. Harnessed data from decades of company’s experience and relationships, creating an interactive GUI and updatable database, enabling any user to optimize dispatch decisions." />
       </div>
     );
   }
