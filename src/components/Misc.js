c63894fc400410276474471502e54d900ece87c8
sofia <sofia@smcf.io>
2019-04-16 18:31:24 -0400

    Added misc

--- src/components/Misc.js
+++ src/components/Misc.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import '../styles/Misc.css';
+import menger from '../images/menger.jpg';
+// import me from '../images/portrait-me.jpg';
+// import om from '../images/portrait-om.jpg';
+// import xan from '../images/portrait-xan.jpg';
+// import xannie from '../images/portrait-xan-photo.jpg';
 
 const Misc = () => {
     return (
+      <div>
       <h1>Misc</h1>
+      <div>
+      <h3>Menger sponge cake</h3> <img src={menger} alt="menger sponge cake" />
+      </div>
+      </div>
     );
 }
 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import '../styles/Portfolio.css';
-
+import map from '../images/map.png';
+import addon from '../images/addon.png';
 class Project extends Component {
   render() {
     return (
       <div>
       <h3><a href={this.props.url}>{this.props.name}</a></h3>
-      <span>{this.props.technologies}</span>
+      <span>{this.props.technologies}</span> <img src={this.props.image} alt={this.props.alt}/>
       <p>{this.props.description}</p>
       </div>
     );
@@ -18,8 +19,8 @@ class Portfolio extends Component {
     return (
       <div>
       <h1>Portfolio</h1>
-      <Project name="Courier Map" url="https://smcf.nfshost.com/map/" technologies="JavaScript, Google Maps JavaScript API, MariaDB, PHP, HTML/CSS" description="Developed quote generator and subcontractor dispatch tool for national courier network. Harnessed data from decades of company’s experience and relationships, creating an interactive GUI and updatable database, enabling any user to optimize dispatch decisions." />
-      <Project name="Accounting Add-on" url="https://github.com/sofiacf/osc-billing" technologies="Google Apps Script" description="Developed quote generator and subcontractor dispatch tool for national courier network. Harnessed data from decades of company’s experience and relationships, creating an interactive GUI and updatable database, enabling any user to optimize dispatch decisions." />
+      <Project name="Courier Map" image={map} url="https://smcf.nfshost.com/map/" alt="courier map" technologies="JavaScript, Google Maps JavaScript API, MariaDB, PHP, HTML/CSS" description="Developed quote generator and subcontractor dispatch tool for national courier network. Harnessed data from decades of company’s experience and relationships, creating an interactive GUI and updatable database, enabling any user to optimize dispatch decisions." />
+      <Project name="Accounting Add-on" image={addon} alt="sheets add-on" url="https://github.com/sofiacf/osc-billing" technologies="Google Apps Script" description="Developed quote generator and subcontractor dispatch tool for national courier network. Harnessed data from decades of company’s experience and relationships, creating an interactive GUI and updatable database, enabling any user to optimize dispatch decisions." />
       </div>
     );
   }
