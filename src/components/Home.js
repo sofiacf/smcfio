8e3904d3f9e9df504932d1321fb8e602aeff94ee
sofia <sofia@smcf.io>
2019-04-24 18:27:43 -0400

    Homepage updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,21 +6,25 @@ class Home extends Component {
   render() {
     return (
       <div>
-      <div className="welcome">
-        <div className="about">
-          <p>Thanks for visiting smcf.io, the homepage of Sofia Chandler-Freed.</p>
-          <p>I'm currently open for developer roles in Boston.'</p>
-          <p>Feel free to view my resume, portfolio, and other stuff here. If you like what you see, <a class="email" href="mailto:sofia@smcf.io">send me an email</a>!</p>
-        </div>
-        <div className="specialties">
-          <p>My specialties:</p>
-          <li>Javascript</li>
-          <li>HTML/CSS</li>
-          <li>PHP, MariaDB</li>
-          <li>Google Apps Script</li>
-        </div>
-      </div>
-      <p>-Sofia</p>
+      <table><tbody><tr>
+        <td>
+            <p>Thanks for visiting smcf.io, the homepage of Sofia Chandler-Freed.</p>
+            <p>I'm currently open for developer roles in Boston.'</p>
+            <p>Feel free to view my resume, portfolio, and other stuff here. If you like what you see, <a class="email" href="mailto:sofia@smcf.io">send me an email</a>!</p>
+            <p>-Sofia</p>
+      </td>
+      <td>
+          <p>
+          Specialties:
+          </p>
+          <ul>
+            <li>JavaScript, HTML/CSS, React</li>
+            <li>TypeScript, PHP, MySQL</li>
+            <li>Aurora, MariaDB, LAMP</li>
+            <li>AWS, Python, Git, Arduino</li>
+          </ul>
+      </td>
+      </tr></tbody></table>
       </div>
     );
   }
