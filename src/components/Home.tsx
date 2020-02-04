93801debccc065cf45bdd715db0cfc95968e7e8f
sofia <dev@smcf.io>
2020-02-03 22:14:25 -0500

    Update skills

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,29 +1,27 @@
 import React from 'react'
 import '../styles/Home.css'
 
-export const Home = () =>
-    <div>
-        <table>
-            <tbody>
-            <tr>
-                <td style={ { width: '60%' } }>
-                    <p>Welcome to smcf.io, homepage of Sofia Chandler-Freed.</p>
-                    <p>Please enjoy my portfolio, writing, resume, other odds and ends while you're here.</p>
-                    <p>Inclined to comment or inquire? Feel free to email me: <a className="email"
-                                                                                 href="mailto:sofia@smcf.io">sofia@smcf.io</a>!
-                    </p>
-                    <p>-Sofia</p>
-                </td>
-                <td>
-                    <p>Specialties:</p>
-                    <ul>
-                        <li>TypeScript, React, HTML/CSS</li>
-                        <li>Java, Spring, JPA, MySQL</li>
-                        <li>AWS, PCF, Git, TDD, XP</li>
-                        <li>Delivering reliable innovation</li>
-                    </ul>
-                </td>
-            </tr>
-            </tbody>
-        </table>
-    </div>
+export const Home = () => <main>
+    <table>
+        <tbody>
+        <tr>
+            <td style={ { width: '60%' } }>
+                <p>Welcome to smcf.io, homepage of Sofia Chandler-Freed.</p>
+                <p>Please enjoy my portfolio, writing, resume, other odds and ends while you're here.</p>
+                <p>Inclined to comment or inquire? <a className="email" href="mailto:sofia@smcf.io">Email me</a>!
+                </p>
+                <p>-Sofia</p>
+            </td>
+            <td>
+                <p>Specialties:</p>
+                <ul>
+                    <li>React, Typescript, HTML/CSS</li>
+                    <li>Java, Spring Boot, JPA, MySQL</li>
+                    <li>AWS, PCF, Git, TDD, XP</li>
+                    <li>Delivering reliable innovation</li>
+                </ul>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</main>
